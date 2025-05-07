37d6ecaf5f5bfcfeef80cf18940721e6ae3c12b5
Ryan Hirte <rhirte@ewu.edu>
2025-05-07 11:56:24 -0700

    Chasing works now

--- Assets/Monster/UpdatedMonster.cs
+++ Assets/Monster/UpdatedMonster.cs
@@ -5,33 +5,38 @@ using UnityEngine.AI;
 
 public class UpdatedMonster : MonoBehaviour
 {
+	// settings for monster
 	public Transform player;
-	public float walkSpeed, chaseSpeed;
-	private bool isHunting, isChasing;
+	public float walkSpeed, chaseSpeed, chaseRange, chaseTime;
 	public List<Transform> destinations;
 
+	// Agent and navigation
 	private Transform destination;
 	private int randNum;
 	private NavMeshAgent agent;
+	private bool isHunting, isChasing;
 
-	private float waitTime = 3f;
-	private bool isWaiting = false;
+	
 
 	void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
-		isHunting = true;
-		agent.speed = walkSpeed;
+		StopChasing();
 	}
 
 	void Update()
 	{
+		CheckPlayerProximity();
 		if (isHunting)
 		{
 			if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
 			{
 				PickNewDestination();
 			}
+		}
+		if (isChasing)
+		{
+			StartChasing();
 		}
 	}
 
@@ -45,6 +50,37 @@ public class UpdatedMonster : MonoBehaviour
 		randNum = Random.Range(0, destinations.Count);
 		destination = destinations[randNum];
 		agent.destination = destination.position;
+	}
+
+	void StartChasing()
+	{
+		Debug.Log("Chasing player");
+		agent.speed = chaseSpeed;
+		agent.destination = player.position;
+	}
+
+	void StopChasing()
+	{
+		Debug.Log("Stopping the chase...");
+        isChasing = false;
+        isHunting = true;
+        agent.speed = walkSpeed; 
+        PickNewDestination(); 
+	}
+
+	void CheckPlayerProximity()
+	{
+		float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+		if (distanceToPlayer < chaseRange)
+		{
+			isChasing = true;
+			isHunting = false;
+		}
+		else
+		{
+			isChasing = false;
+			isHunting = true;
+		}
 	}
 
 }
